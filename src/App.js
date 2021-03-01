65eb69f047b0a1d41e1d1fbe31b57ef796f49e48
Marcus Fernandez <marcustfernandez@gmail.com>
2021-03-01 13:29:00 -0500

    added comments, renamed variables, edited SCSS

--- src/App.js
+++ src/App.js
@@ -9,43 +9,49 @@ function App() {
   const [textBarValue, setTextBarValue] = useState("")
 
   // the current value of the to-do list in state
-  const [currentListState, setCurrentListState] = useState([{ id: 0, value: "Use the input field above to add todo items!", isComplete: false }])
+  const [todoList, setTodoList] = useState([{ id: 0, value: "Use the input field above to add todo items!", isComplete: false }])
 
+  // the current value of the done list in state
   const [doneList, setDoneList] = useState([])
 
   // running count of the ammount of created to-dos so that each to-do has a unique id
   const [idCounter, setIdCounter] = useState(1)
   
-  const checkOffItem = e => {
-    if (e.target.checked) {
-      const clickedItem = currentListState.filter(item => item.id === parseInt(e.target.id))[0]
+  /**
+   * checks if the checkbox is checked.
+   * if true (meaning it was just checked off), moves the item from the todo array to the done array and removes it from the todo array.
+   * else it does the opposite, pushes the clicked item to the todo array and removes it from the done array.
+   */
+  const checkOffItem = event => {
+    if (event.target.checked) {
+      const clickedItem = todoList.filter(item => item.id === parseInt(event.target.id))[0]
       clickedItem.isComplete = true
       doneList.push(clickedItem)
-      setCurrentListState(currentListState.filter(item => item.id !== clickedItem.id))
+      setTodoList(todoList.filter(item => item.id !== clickedItem.id))
     } else {
-      const clickedItem = doneList.filter(item => item.id === parseInt(e.target.id))[0]
+      const clickedItem = doneList.filter(item => item.id === parseInt(event.target.id))[0]
       clickedItem.isComplete = false
-      currentListState.push(clickedItem)
+      todoList.push(clickedItem)
       setDoneList(doneList.filter(item => item.id !== clickedItem.id))
     }
   }
 
   // handles the input into the top text bar
-  const textBarInputHandler = e => setTextBarValue(e.target.value)
+  const textBarInputHandler = event => setTextBarValue(event.target.value)
 
   // when the "X" to the right of an item is clicked this updates the list with a filter to delete it.
-  // it runs a check to get the tab id to check which array to filter.
-  const removeListItem = e => {
-    if(e.target.parentElement.parentElement.parentElement.id === "todo-tab") {
-      setCurrentListState(currentListState.filter(item => item.id !== parseInt(e.target.id)))
+  // it runs a check to get the tab id to check which array to filter (that's that parentElement.parentElement.parentElement bit lol).
+  const removeListItem = event => {
+    if(event.target.parentElement.parentElement.parentElement.id === "todo-tab") {
+      setTodoList(todoList.filter(item => item.id !== parseInt(event.target.id)))
     } else {
-      setDoneList(doneList.filter(item => item.id !== parseInt(e.target.id)))
+      setDoneList(doneList.filter(item => item.id !== parseInt(event.target.id)))
     }
   }
 
   // when the "+" to the right of the input bar is clicked, the value in the input is pushed to the list, the idCounter is incremented, and the text bar is cleared.
   const clickFunction = () => {
-    currentListState.push({ id: (idCounter), value: textBarValue, isComplete: false })
+    todoList.push({ id: (idCounter), value: textBarValue, isComplete: false })
     setIdCounter(idCounter + 1)
     setTextBarValue("")
   }
@@ -62,7 +68,7 @@ function App() {
         clickFunction={clickFunction}
       />
       <Tabs
-        currentListState={currentListState}
+        todoList={todoList}
         doneList={doneList}
         removeListItem={removeListItem}
         checkOffItem={checkOffItem}
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ListItem from "../ListItem/ListItem"
 import "./Tab.scss"
 
-// passing props down
-const Tab = ({ currentListState, removeListItem, doneList, checkOffItem }) => {
-    const changeTab = e=>{
-        e.target.className="active-tab"
-        e.target.nextSibling ? (e.target.nextSibling.className = "inactive-tab") : (e.target.previousSibling.className = "inactive-tab")
-        if ( e.target.id === "todo-tab-btn" ) {
+const Tab = ({ todoList, removeListItem, doneList, checkOffItem }) => {
+
+    // not sure if this is the way I should do this. It works and it doesn't re-render the DOM, so I think it's fine?
+    const changeTab = e => {
+
+        e.target.className = "active-tab" // changes the tab class of the clicked tab to "active-tab"
+
+        // checks to see if a previous or next tab exists and sets its class to "inactive-tab"
+        e.target.nextSibling ? (e.target.nextSibling.className = "inactive-tab") : (e.target.previousSibling.className = "inactive-tab") 
+        
+        // this changes the display style of the the todo and done tabs so that the appropriate one is displayed.
+        if (e.target.id === "todo-tab-btn") {
             document.getElementById("todo-tab").style.display = "block"
             document.getElementById("done-tab").style.display = "none"
         } else {
@@ -15,46 +21,52 @@ const Tab = ({ currentListState, removeListItem, doneList, checkOffItem }) => {
             document.getElementById("todo-tab").style.display = "none"
         }
     }
+
+
+    /**
+     * this renders both the todo and done lists. I figured keeping both rendered was fine since it's looping through and mapping an array each time it renders, so leaving them
+     * in the DOM but hidden when not being viewed was a way to avoid extra re-rendering. Not that it really matters with an app this small, but still.
+     * */ 
     return (
         <div className="tabs-wrapper">
             <nav>
-            <div className="active-tab" onClick={changeTab} id="todo-tab-btn">ToDo</div>
-            <div className="inactive-tab" onClick={changeTab} id="done-tab-btn">Done</div>
+                <div className="active-tab" onClick={changeTab} id="todo-tab-btn">ToDo</div>
+                <div className="inactive-tab" onClick={changeTab} id="done-tab-btn">Done</div>
             </nav>
             <div className="tab" id="todo-tab">
                 <ul className="todosList" id={`todos-todo-tab`}>{
 
                     // Mapping the currentListState to ListItem components with their respecive id's
-                    currentListState.map(listItem => {
+                    todoList.map(listItem => {
                         return (
                             <ListItem
-                            isChecked={false}
-                            key={listItem.id}
-                            className="listItem"
-                            id={`listItem-${listItem.id}`}
-                            removeListItem={removeListItem}    // passing this function ALL the way down.
-                            checkOffItem={checkOffItem}
-                            closeBtnKey={listItem.id}
-                            innerText={listItem.value}
-                            />
-                            )
-                        })
-                        
-                    }
-                </ul>
-            </div>
-            <div className="tab" id="done-tab" style={{display:"none"}}>
-                <ul className="todosList" id={`todos-done-tab`}>{
-                    
-                    // Mapping the currentListState to ListItem components with their respecive id's
-                    doneList.map(listItem => {
-                        return (
-                            <ListItem
-                            isChecked={true}
+                                isChecked={false}
                                 key={listItem.id}
                                 className="listItem"
                                 id={`listItem-${listItem.id}`}
-                                removeListItem={removeListItem}    // passing this function ALL the way down.
+                                removeListItem={removeListItem}
+                                checkOffItem={checkOffItem}
+                                closeBtnKey={listItem.id}
+                                innerText={listItem.value}
+                            />
+                        )
+                    })
+
+                }
+                </ul>
+            </div>
+            <div className="tab" id="done-tab" style={{ display: "none" }}>
+                <ul className="todosList" id={`todos-done-tab`}>{
+
+                    // Mapping the doneList to ListItem components with their respecive id's
+                    doneList.map(listItem => {
+                        return (
+                            <ListItem
+                                isChecked={true}
+                                key={listItem.id}
+                                className="listItem"
+                                id={`listItem-${listItem.id}`}
+                                removeListItem={removeListItem}
                                 closeBtnKey={listItem.id}
                                 checkOffItem={checkOffItem}
                                 innerText={listItem.value}
